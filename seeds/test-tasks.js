838c699ceae274d89f55b65c93452d5f985cdf9f
Josh Liuaana <98368294+josh-liuaana@users.noreply.github.com>
2022-03-28 12:34:48 +1300

    update task names, test switch statements

--- seeds/test-tasks.js
+++ seeds/test-tasks.js
@@ -8,9 +8,9 @@ exports.seed = function (knex) {
     .del()
     .then(() => {
       return knex('todos').insert([
-        { id: 1, task: 'task' },
-        { id: 2, task: 'rowValue2' },
-        { id: 3, task: 'rowValue3' },
+        { id: 1, task: 'vaccuum' },
+        { id: 2, task: 'buy groceries' },
+        { id: 3, task: 'tidy wardrobe' },
       ])
     })
 }
