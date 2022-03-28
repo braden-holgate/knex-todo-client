b82fcc39b93613a1f9e3c0885ba095bffd17fe14
Muer Zhang <muerzhangnz@gmail.com>
2022-03-28 13:16:19 +1300

    Add a migration for the todos table

--- migrations/20220328000742_todos.js
+++ migrations/20220328000742_todos.js
@@ -2,10 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {}
+exports.up = function (knex) {
+  return knex.schema.createTable('todos', (table) => {
+    table.increments('id')
+    table.string('task')
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {}
+exports.down = function (knex) {
+  return knex.schema.dropTable('todos')
+}
