aa48f1dd346614e4e1479104762cb92fcc144a87
Lawrence Goodhead <92251452+Lawrence-gg@users.noreply.github.com>
2022-03-28 21:47:21 +1300

    user can add and remove tasks

--- commands.js
+++ commands.js
@@ -25,6 +25,22 @@ function logError(err) {
   console.error('Uh oh!', err.message)
 }
 
+function deleteTodo(id) {
+  return db.removeTodo(id).then(() => {
+    console.log(`Delete from Todos ID: ${id}`)
+    db.close()
+  })
+}
+
+function newTodo(task) {
+  return db.addTodo(task).then(() => {
+    console.log(`Task added successfully: ${task}`)
+    db.close()
+  })
+}
+
 module.exports = {
   list,
+  deleteTodo,
+  newTodo,
 }
